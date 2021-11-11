af32d8ed6d8001220afdaf22d2f35034bdbc65d1
XiaoyuWant <kamisamarain@gmail.com>
2021-11-11 19:28:26 +0800

    simplify

--- no2_gen_label/gen_label.py
+++ no2_gen_label/gen_label.py
@@ -10,6 +10,7 @@ import torchvision.transforms as transforms
 
 from torchvision import datasets, models, transforms
 from torch.optim.lr_scheduler import MultiStepLR
+from torchvision.datasets import ImageFolder
 import torch.backends.cudnn as cudnn
 import torchvision.models as tv_models
 import torch.optim as optim
@@ -28,46 +29,6 @@ from tqdm import tqdm
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-class ImageFolderMy(torch.utils.data.Dataset):
-    def __init__(self,root,transform,imgsLimited=1000):
-        classes=glob.glob(root+"/*")
-        #print(classes)
-        #classes=classes[:1000]
-        self.transform=transform
-        self.imgs=[]
-        self.labels=[]
-        for i in range(len(classes)):
-            one=classes[i]
-            imgs=glob.glob(one+'/*.jpg')
-            if(len(imgs)>imgsLimited):
-                imgs=imgs[:imgsLimited]
-            #print("img len:",len(imgs))
-            labels=[i for _ in range(len(imgs))]
-            #print("img len:",len(labels))
-            self.imgs+=imgs
-            self.labels+=labels
-    def __getitem__(self,index):
-        img=self.imgs[index]
-        imgname=img
-        label=self.labels[index]
-        img=Image.open(img).convert('RGB')
-        img=self.transform(img)
-        return img,label,imgname
-    def __len__(self):
-        return len(self.labels)
-class DatasetFromAnnos(torch.utils.data.Dataset):
-    def __init__(self,root,transform):
-        csv=pd.read_csv("/root/userfolder/Exp/DDP_Classification_pytorch/CDR/annos.csv",sep=',')
-        self.csv=csv
-    def __getitem__(self,index):
-        #print("FILE:{}".format(self.imgs[index]))
-        img=self.csv.iloc[index]["filepath"]
-        label=self.csv.iloc[index]["label"]
-        img=Image.open(img).convert('RGB')
-        img=self.transform(img)
-        return img,label
-    def __len__(self):
-        return len(print(len(csv)))
 image_transforms = {
             'train':transforms.Compose([
                 #transforms.ToPILImage(),
@@ -98,7 +59,7 @@ def GenerateCleanDataset(model_path,out_file,dataset_path):
         )
     model.load_state_dict(torch.load(model_path))
     model=model.cuda()
-    dataset=ImageFolderMy(root=dataset_path,transform=image_transforms['val'])
+    dataset=ImageFolder(root=dataset_path,transform=image_transforms['val'])
     dataloader= torch.utils.data.DataLoader(dataset=dataset,
                                                batch_size=32,
                                                num_workers=4,
